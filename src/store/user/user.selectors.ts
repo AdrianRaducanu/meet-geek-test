68cb21d5ce329ddae799fcc5a80c24539ca0d896
AdrianRaducanu <48106661+AdrianRaducanu@users.noreply.github.com>
2024-08-21 11:27:45 +0300

    final touch

--- src/store/user/user.selectors.ts
+++ src/store/user/user.selectors.ts
@@ -4,7 +4,6 @@ import {UserState} from "./user.state";
 
 export const selectUserState = (state: AppState) => state.user;
 
-// Step 2: Create a selector for the username
 export const selectUsername = createSelector(
   selectUserState,
   (state: UserState) => state.username
